0884440cb2504d86b541b4d1e59a989fa81d090b
Vincent Nguyen <kureikain@gmail.com>
2011-07-15 04:50:21 -0700

    added replaceAll function for String class

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,25 @@
+String.prototype.replaceAll = function(
+    strTarget, // The substring you want to replace
+    strSubString // The string you want to replace in.
+    ){
+    var strText = this;
+    var intIndexOfMatch = strText.indexOf( strTarget );
+ 
+    // Keep looping while an instance of the target string
+    // still exists in the string.
+    while (intIndexOfMatch != -1){
+        // Relace out the current instance.
+        strText = strText.replace( strTarget, strSubString )
+ 
+        // Get the index of any next matching substring.
+        intIndexOfMatch = strText.indexOf( strTarget );
+    }
+ 
+    // Return the updated string with ALL the target strings
+    // replaced out with the new substring.
+    return( strText );
+}
+
 window.wfapp = {};
 (function ($) {
     window.wfapp = {
