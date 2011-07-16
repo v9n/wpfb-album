6c218375a10641dfb3b6bb52bf526fa9d2539a7c
Vincent Nguyen <kureikain@gmail.com>
2011-07-16 03:42:35 -0700

    Updated nivo

--- helper/gallery/nivo.php
+++ helper/gallery/nivo.php
@@ -1,23 +1,36 @@
 <?php
-
 /*
  *  A venture of Axcoto http://axcoto.com
  *  by Vincent Nguyen <info@axcoto.com>
  */
 
 class WfalbumHelperGalleryNivo extends WfalbumHelperGallery {
+
     static public function info() {
         return array(
             'id' => 'nivo',
             'name' => 'Nivo Slider'
         );
     }
-    
+
     public function render() {
         
     }
-    
+
     static public function bootstrap() {
         ;
     }
+
+    /**
+     * Render preference setting box for this plugin
+     */
+    static function preference() {
+        ?>        
+        <input type="text" name="height" value="" />
+        <label>Auto Play</label>
+        <input type="checkbox" name="autoplay" value="" />
+        <label>Show image nav</label>
+        <?php
+    }
+
 }
