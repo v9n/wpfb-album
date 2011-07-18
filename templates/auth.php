13621f25ecf73dca99f505fa7cf5a9e30970089c
Vincent Nguyen <kureikain@gmail.com>
2011-07-17 17:38:16 -0700

    Change not authorized text

--- templates/auth.php
+++ templates/auth.php
@@ -1,9 +1,12 @@
 <div id="wfalbum_form" style="display:none;">
     <div class="wrap">
         <p>
-            System cannot access your facebook photo! Maybe you have not authorize our application!
+            System cannot access your facebook photo! Maybe you have not authorized our application!
+            This authorizing is to give this app permission to read your facebook photo!
+            After authorized, you can come back this page to use this plugin!
             <br />
-            <a class="button" href="<?php echo $fb->getAuthUrl() ?>">Start to authorize</a>
+            <a class="button" target="_blank" onlick="jQuery.colorbox.close()
+               " href="<?php echo $fb->getAuthUrl() ?>">I am ok! Start to authorize</a>
         </p>
     </div>
 </div>
