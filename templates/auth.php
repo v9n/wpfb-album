98b49086190aa6cc85d344bd18e7794cd196102a
Vincent Nguyen <kureikain@gmail.com>
2011-07-17 18:36:40 -0700

    Update intro text

--- templates/auth.php
+++ templates/auth.php
@@ -2,10 +2,10 @@
     <p>
         System cannot access your facebook photo! Maybe you have not authorized our application!
         This authorizing is to give this app permission to read your facebook photo!
-        After authorized, you can come back this page to use this plugin!
+        After authorized, you can close that facebook window/tab and come back this page to use plugin!
     </p>
     <br />
     <p>
-        <a class="button" target="_blank" href="javascript:window.open('<?php echo $fb->getAuthUrl() ?>')">I am ok! Start to authorize</a>
+        <a class="button" target="_blank" onclick="wfapp.close();" href="<?php echo $fb->getAuthUrl() ?>">I am ok! Start to authorize</a>
     </p>
 </div>
