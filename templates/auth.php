983a7e7641fc32f6460e58f7683a4498a5b74016
Vincent Nguyen <kureikain@gmail.com>
2011-07-17 18:15:24 -0700

    Update text

--- templates/auth.php
+++ templates/auth.php
@@ -1,12 +1,11 @@
-<div id="wfalbum_form" style="display:none;">
-    <div class="wrap">
-        <p>
-            System cannot access your facebook photo! Maybe you have not authorized our application!
-            This authorizing is to give this app permission to read your facebook photo!
-            After authorized, you can come back this page to use this plugin!
-            <br />
-            <a class="button" target="_blank" onlick="jQuery.colorbox.close()
-               " href="<?php echo $fb->getAuthUrl() ?>">I am ok! Start to authorize</a>
-        </p>
-    </div>
+<div id="wf_auth_box">
+    <p>
+        System cannot access your facebook photo! Maybe you have not authorized our application!
+        This authorizing is to give this app permission to read your facebook photo!
+        After authorized, you can come back this page to use this plugin!
+    </p>
+    <br />
+    <p>
+        <a class="button" target="_blank" href="javascript:window.open('<?php echo $fb->getAuthUrl() ?>')">I am ok! Start to authorize</a>
+    </p>
 </div>
