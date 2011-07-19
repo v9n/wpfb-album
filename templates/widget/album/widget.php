b649fb09563d45aff4993323b10cc4b455df1ad0
Vincent Nguyen <kureikain@gmail.com>
2011-07-18 20:07:54 -0700

    Upate widget

--- templates/widget/album/widget.php
+++ templates/widget/album/widget.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
  *  A venture of Axcoto http://axcoto.com
  *  by Vincent Nguyen <info@axcoto.com>
@@ -7,17 +6,41 @@
 ?>
 <?php echo $before_widget; ?>
 <?php $title && print ($before_title . $title . $after_title); ?>
-
-<ul>
+<ul id="<?php echo $this->id ?>_wrap" class="wfalbum_widget">
     <?php
-    if (is_array($groups)) : foreach ($groups as $post) :
+    if (is_array($photos['data'])) : foreach ($photos['data'] as $photo) :
+            ?>
+            <li>
+                <a href="<?php echo $photo['images'][0]['source'] ?>" rel="<?php echo $this->id ?>">
+                    <img src="<?php echo $photo['images'][3]['source'] ?>" />
+                </a>
+            </li>
+            <?php
+        endforeach;
+    endif
     ?>
-            <li><a href="<?php echo get_permalink($post->ID) ?>"><?php echo $post->post_title ?></a></li>
-    <?php
-            endforeach;
-        endif
-    ?>
-    </ul>
-
-
+</ul>
 <?php echo $after_widget; ?>
+<script type="text/javascript">    
+    //<![CDATA[
+    (function ($) {
+        $(document).ready(function () {
+<?php
+switch ($instance['mode']) :
+    case 'supersized':
+        ?>
+                                            
+        <?php
+        break;
+    case 'colorbox':
+    default:
+        ?>
+                            $("a[rel='<?php echo $this->id ?>']").colorbox({slideshow:true});
+        <?php
+        break;
+endswitch;
+?>
+        })
+    })(jQuery)
+    //]]>
+</script>
