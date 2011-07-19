9fc410a454c7536404324041aaa68ba8f08d560d
Vincent Nguyen <kureikain@gmail.com>
2011-07-18 20:07:59 -0700

    Upate widget

--- templates/widget/album/form.php
+++ templates/widget/album/form.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
  *  A venture of Axcoto http://axcoto.com
  *  by Vincent Nguyen <info@axcoto.com>
@@ -11,20 +10,32 @@
 </p>
 
 <p>
-    <label for="<?php echo $this->get_field_id('quantity'); ?>"><?php _e('How many photos?'); ?></label>
+    <label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Choose Album:'); ?></label>
+    <select class="widefat" id="<?php echo $this->get_field_id('album_id'); ?>" name="<?php echo $this->get_field_name('album_id'); ?>">
+        <?php foreach ($albums['data'] as $album) : ?>
+        <option value="<?php echo $album['id'] ?>" <?php $album['id']==$album_id && print('selected="selected"')?>  ><?php echo $album['name'] ?></option>         
+            <?php
+        endforeach;
+        ?>
+    </select>
+</p>
+
+<p>
+    <label for="<?php echo $this->get_field_id('quantity'); ?>"><?php _e('How many photos to display?');
+        ?></label>
     <input class="widefat" id="<?php echo $this->get_field_id('quantity'); ?>" name="<?php echo $this->get_field_name('quantity'); ?>" type="text" value="<?php echo $quantity; ?>" />
 </p>
 
 <p>
     <label for="<?php echo $this->get_field_id('size'); ?>"><?php _e('Thumbnail Size'); ?></label>
-    <input class="widefat" id="<?php echo $this->get_field_id('quantity'); ?>" name="<?php echo $this->get_field_name('quantity'); ?>" type="text" value="<?php echo $quantity; ?>" />
-    <input class="widefat" id="<?php echo $this->get_field_id('quantity'); ?>" name="<?php echo $this->get_field_name('quantity'); ?>" type="text" value="<?php echo $quantity; ?>" />
+    <input class="widefat medium" id="<?php echo $this->get_field_id('width'); ?>" name="<?php echo $this->get_field_name('width'); ?>" type="text" value="<?php echo $width; ?>" />
+    <input class="widefat medium" id="<?php echo $this->get_field_id('height'); ?>" name="<?php echo $this->get_field_name('height'); ?>" type="text" value="<?php echo $height; ?>" />
 </p>
 
 <p>
-    <label for="<?php echo $this->get_field_id('type'); ?>"><?php _e('Display Type:'); ?></label>
-    <select class="widefat" id="<?php echo $this->get_field_id('type'); ?>" name="<?php echo $this->get_field_name('type'); ?>" >
-        <option <?php ($type == 'myactive') && print('selected="selected"'); ?>  value="myactive">Colorbox</option>
-        <option <?php ($type == 'myactive') && print('selected="selected"'); ?>  value="myactive">Supersized!</option>
+    <label for="<?php echo $this->get_field_id('mode'); ?>"><?php _e('Display Type:'); ?></label>
+    <select class="widefat" id="<?php echo $this->get_field_id('mode'); ?>" name="<?php echo $this->get_field_name('mode'); ?>" >
+        <option <?php ($mode == 'colorbox') && print('selected="selected"'); ?>  value="colorbox">Colorbox</option>
+        <option <?php ($mode == 'supersize') && print('selected="selected"'); ?>  value="supersize">Supersized!</option>
     </select>
 </p>
