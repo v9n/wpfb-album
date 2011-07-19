1a539c5a0132e28657412b0a85e6f41703706bae
Vincent Nguyen <kureikain@gmail.com>
2011-07-19 16:57:45 -0700

    Remove junk file which is not belong too this plugin

--- templates/email/invite.php
+++
@@ -1,8 +0,0 @@
-<?php
-return "
-Hello, {name}
-I would like to invite you become a member of group and make donation on this group!
-<a href=\"{group_link}\">Click here to join in and donate via our website</a>
-If you have a facebook account, you can go here <a href=\"{facebook_link}\">Click here to join in and donate via facebook</a>
-Thank you!
-";
