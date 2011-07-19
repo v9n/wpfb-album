cb98f8080a4ddcd9f8bbf51288707c586b795f78
Vincent Nguyen <kureikain@gmail.com>
2011-07-18 19:35:30 -0700

    Added auth for widget

---
+++ templates/widget/album/auth.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ *  A venture of Axcoto http://axcoto.com
+ *  by Vincent Nguyen <info@axcoto.com>
+ */
+?>
+<p>
+    System cannot access your facebook photo! Maybe you have not authorized our application!
+    This authorizing is to give this app permission to read your facebook photo!
+    After authorized, you can close that facebook window/tab and come back this page to use plugin!
+</p>
+<br />
+<p>
+    <a class="button" href="<?php echo $fb->getAuthUrl() ?>">I am ok! Start to authorize</a>
+</p>
