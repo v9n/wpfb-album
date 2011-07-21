7f11619ef5b7f4fee5f4ec246803e848b9b31f8d
Vincent Nguyen <kureikain@gmail.com>
2011-07-21 11:32:37 -0700

    Added options for setting page for facebook app

--- option/core.php
+++ option/core.php
@@ -11,22 +11,20 @@ class WfalbumOptionCore {
         $optionName = Wfalbum::singleton()->optionName;
         $defaultOption = array(
             'fp_cache_time' => 5000,
+            'fp_app_secret' => '',
+            'fp_app_id' => '',
         );
         $option = get_option($optionName, false);
         if (!$option) {
             update_option($optionName, $defaultOption);
         }
-        
+
         //Use our own setting page
         register_setting($optionName . '_group', $optionName);
         add_settings_section($optionName . '_facebook', 'Facebook App Setting', array('WfalbumOptionCore', 'setting_overview_facebook'), 'wfalbum_facebook');
+        add_settings_field('fb_app_id', 'Facebook App ID/Key', array('WfalbumOptionCore', 'setting_control_fb_app_id'), 'wfalbum_facebook', $optionName . '_facebook');
+        add_settings_field('fp_app_secret', 'Facebook App Secret', array('WfalbumOptionCore', 'setting_control_fb_app_secret'), 'wfalbum_facebook', $optionName . '_facebook');
         add_settings_field('fp_cache_time', 'Cache Time', array('WfalbumOptionCore', 'setting_control_fp_cache_time'), 'wfalbum_facebook', $optionName . '_facebook');
-        
-        
-        //USE buildin Setting > General page
-//        register_setting('general', $optionName);
-//        add_settings_section($optionName . '_facebook_op', 'Facebook App Setting', array('WfalbumOptionCore', 'setting_overview_facebook'), 'general');
-//        add_settings_field('fb_app_id', 'Facebook App ID', array('WfalbumOptionCore', 'setting_control_fb_app_id'), 'general', $optionName . '_facebook_op');
         
     }
 
@@ -50,41 +48,41 @@ class WfalbumOptionCore {
     public static function setting_control($field='') {
         $optionName = Wfalbum::singleton()->optionName;
         $optionValue = get_option($optionName);
-?>
+        ?>
         <input id="<?php echo $field ?>" name="<?php echo $optionName ?>[<?php echo $field ?>]" class="regular-text" value="<?php echo $optionValue[$field]; ?>" />
-<?php
+        <?php
     }
 
-        /**
-         * Render setting form
-         */
-        public static function setting_form() {
-            $optionName = Wfalbum::singleton()->optionName;
-?>
-            <div class="wrap">
-    <?php screen_icon("options-general"); ?>
+    /**
+     * Render setting form
+     */
+    public static function setting_form() {
+        $optionName = Wfalbum::singleton()->optionName;
+        ?>
+        <div class="wrap">
+            <?php screen_icon("options-general"); ?>
             <h2><?php echo __('WP FB Album Settings') ?></h2>
             <form action="options.php" method="post">
 
                 <?php settings_fields($optionName . '_group'); ?>
                 <?php do_settings_sections('wfalbum_facebook'); ?>
 
-            <p class="submit">
-                <input name="Submit" type="submit" class="button-primary" value="<?php esc_attr_e('Save Changes'); ?>" />
-            </p>
-        </form>
-    </div>
-<?php
-        }
-
-        public static function __callStatic($name, $args) {
-            if (substr($name, 0, 16) == 'setting_control_') {
-                echo call_user_func(array('WfalbumOptionCore', 'setting_control'), substr($name, 16));
-            } elseif (substr($name, 0, 17) == 'setting_overview_') {
-                echo call_user_func(array('WfalbumOptionCore', 'setting_overview'), substr($name, 17));
-            }
-            return '';
-        }
-
+                <p class="submit">
+                    <input name="Submit" type="submit" class="button-primary" value="<?php esc_attr_e('Save Changes'); ?>" />
+                </p>
+            </form>
+        </div>
+        <?php
     }
+
+    public static function __callStatic($name, $args) {
+        if (substr($name, 0, 16) == 'setting_control_') {
+            echo call_user_func(array('WfalbumOptionCore', 'setting_control'), substr($name, 16));
+        } elseif (substr($name, 0, 17) == 'setting_overview_') {
+            echo call_user_func(array('WfalbumOptionCore', 'setting_overview'), substr($name, 17));
+        }
+        return '';
+    }
+
+}
 ?>
